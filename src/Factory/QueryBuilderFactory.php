d959965acdf14be4f917d1b02d9444bea42293ab
rolandodg <jesushs80@gmail.com>
2017-04-29 21:07:34 +0200

    Created Factory for building Mysql query builder

--- src/Factory/QueryBuilderFactory.php
+++ src/Factory/QueryBuilderFactory.php
@@ -8,5 +8,13 @@ use Phaba\DatabaseManager\QueryBuilder;
 
 abstract class QueryBuilderFactory
 {
-    abstract public function createSelectQueryBuilder(): QueryBuilder;
+    abstract public function createSelectQueryBuilder(
+        string $table,
+        array $fields = [],
+        string $where = null,
+        array $group = [],
+        array $order = [],
+        int $limit = null,
+        int $offSet = null
+    ): QueryBuilder;
 }
---
+++ tests/Factory/Mysql/MysqlQueryBuilderFactoryImpTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phaba\DatabaseManager\Test\Factory\Mysql;
+
+use Phaba\DatabaseManager\Factory\Mysql\MysqlQueryBuilderFactoryImp;
+use Phaba\DatabaseManager\Mysql\Query\SelectMysqlQueryBuilderImp;
+use PHPUnit\Framework\TestCase;
+
+class MysqlQueryBuilderFactoryImpTest extends TestCase
+{
+    /**
+     * @var MysqlQueryBuilderFactoryImp
+     */
+    private $factory;
+
+    public function setUp()
+    {
+        $this->factory = new MysqlQueryBuilderFactoryImp();
+    }
+
+    public function testCanCreateMysqlSelectQueryBuilder(): void
+    {
+        $selectBuilder = $this->factory->createSelectQueryBuilder('tableTest', array('tableField'));
+        $this->assertInstanceOf(SelectMysqlQueryBuilderImp::class, $selectBuilder);
+    }
+}
