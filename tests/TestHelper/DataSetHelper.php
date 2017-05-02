6bd74c21ddc6fcb9dfef0efda9d37cf404ee3038
rolandodg <jesushs80@gmail.com>
2017-05-02 19:04:12 +0200

    Created DBUnit DataSet Test Helper with:  - function for convert DBUnit DataSetTable to Associative Array  - function for creating a DBUnit QueryDataSet

---
+++ tests/TestHelper/DataSetHelper.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phaba\DatabaseManager\Test\TestHelper;
+
+use PHPUnit\DbUnit\DataSet\AbstractTable;
+use PHPUnit\DbUnit\DataSet\AbstractDataSet;
+use PHPUnit\DbUnit\DataSet\QueryDataSet;
+
+/**
+ * Helper class for DBUnit DataSet (used for database integration tests)
+ *
+ * @package Phaba\DatabaseManager\Test\TestHelper
+ */
+class DataSetHelper
+{
+    /**
+     * Convert a DBUnit DataSet Table to pair field=>value array
+     *
+     * @param AbstractTable $dataSet DBUnit DataSet Default Table for converting to array
+     * @return array
+     */
+    public static function dataSetTableToAssociativeArray(AbstractTable $dataSet): array
+    {
+        $results = [];
+        for ($i = 0; $i < $dataSet->getRowCount(); $i++) {
+            $results[] = $dataSet->getRow($i);
+        }
+
+        return $results;
+    }
+
+    public function createQueryDataSet(
+        $conn,
+        string $table,
+        array $fields = [],
+        string $where = '',
+        array $group = [],
+        array $order = [],
+        int $limit = -1,
+        int $offset = -1
+    ): AbstractDataSet {
+        $queryHelper = new QueryHelper();
+        $dataSet = new QueryDataSet($conn);
+        $dataSet->addTable(
+            $table,
+            $queryHelper->buildQueryString($table, $fields, $where, $group, $order, $limit, $offset)
+        );
+        return $dataSet;
+    }
+}
