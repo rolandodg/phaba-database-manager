d805b7d95bfb5dcb3a6452ee2103028be36d9c09
rolandodg <jesushs80@gmail.com>
2017-05-02 17:42:32 +0200

    Added ORDER BY and LIMIT values for UPDATE and DELETE withint DatabaseQuery interface. Refactored SELECT query within DatabaseQuery interface for not using null values by default.

--- src/DatabaseQuery.php
+++ src/DatabaseQuery.php
@@ -18,21 +18,21 @@ interface DatabaseQuery
      *
      * @param string $table Table from where get data
      * @param array $fields Fields contained in results
-     * @param string|null $where Where clause for filtering results
+     * @param string $where Where clause for filtering results
      * @param array $group Fields by which results will be grouped
      * @param array $order Fields by which results will be sorted
-     * @param int|null $limit Max number of rows contained in results
-     * @param int|null $offSet Number of firsts rows to exclude from results
+     * @param int $limit Max number of rows contained in results
+     * @param int $offSet Number of firsts rows to exclude from results
      * @return QueryResult
      */
     public function select(
         string $table,
         array $fields = [],
-        string $where = null,
+        string $where = '',
         array $group = [],
         array $order = [],
-        int $limit = null,
-        int $offSet = null
+        int $limit = -1,
+        int $offSet = -1
     ): QueryResult;
 
     /**
@@ -49,19 +49,29 @@ interface DatabaseQuery
      *
      * @param string $table Table where update row
      * @param array $data Pair field=>value array with fields and values to update
-     * @param string|null $where Where clause for filtering rows to update
+     * @param string $where Where clause for filtering rows to update
+     * @param array $order Fields by which rows will be sorted
+     * @param int $limit Max number of affected rows
      * @return QueryResult
      */
-    public function update(string $table, array $data, string $where = null): QueryResult;
+    public function update(
+        string $table,
+        array $data,
+        string $where = '',
+        array $order = [],
+        int $limit = -1
+    ): QueryResult;
 
     /**
      * Delete database existing rows
      *
      * @param string $table Table where delete rows
-     * @param string|null $where Where clause for filtering rows to delete
+     * @param string $where Where clause for filtering rows to delete
+     * @param array $order Fields by which rows will be sorted
+     * @param int $limit Max number of affected rows
      * @return QueryResult
      */
-    public function delete(string $table, string $where = null): QueryResult;
+    public function delete(string $table, string $where = '', array $order = [], int $limit = -1): QueryResult;
 
     /**
      * Execute an free database query
